6f996ed706fc25c51e30c58e66d1e2cd3e242e90
Ivan Belokobylskiy <belokobylskij@gmail.com>
2019-02-15 20:48:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# st7789mpy
+Slow micropython driver for 240x240 ST7789 display without CS pin from Ali Express
---
+++ test_st7789.py
@@ -0,0 +1,28 @@
+# code for micropython 1.10 on esp8266
+
+import random
+
+import machine
+import st7789
+import time
+
+
+def main():
+    spi = machine.SPI(1, baudrate=40000000, polarity=1)
+    display = st7789.ST7789(
+        spi, 240, 240,
+        reset=machine.Pin(5, machine.Pin.OUT),
+        dc=machine.Pin(2, machine.Pin.OUT),
+    )
+    display.init_display()
+
+    while True:
+        display.fill(
+            st7789.color565(
+                random.getrandbits(8),
+                random.getrandbits(8),
+                random.getrandbits(8),
+            ),
+        )
+        # Pause 2 seconds.
+        time.sleep(2)
